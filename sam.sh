597b80b7764d26482e9acbf7c8fcf6343f4b627d
karinvk <75458512+karinvk@users.noreply.github.com>
2025-11-07 12:18:32 +0100

    Create sam.sh

---
+++ sam.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+PY=python3
+
+#==============================================================================
+ANN="examples/annoations.json"                 # path to JSON annotation file
+CROPS="../crops"                        # directory to save SAM-segmented images
+DEVICE="cuda:0"                                                        # device
+#==============================================================================
+
+$PY ../src/segment.py \
+    --annotation-file "$ANN" \
+    --output-dir "$CROPS" \
+    --device "$DEVICE" \
